494b886dfb0d92b8ec3586702c863437a36e48f5
Vitroze <azouaouiaziz4@gmail.com>
2025-11-21 10:31:25 +0100

    Implementation of the Reload function, which allows for instant repair with compatibility with admin menus, the native Garry's Mod function, and the ability to determine whether or not it is in solo mode.

--- lua/weapons/glide_repair.lua
+++ lua/weapons/glide_repair.lua
@@ -125,6 +125,75 @@ end
 function SWEP:SecondaryAttack()
 end
 
+hook.Add( "InitPostEntity", "GLide_RepairSWEP::CAMI", function()
+    if not CAMI or not CAMI.RegisterPrivilege then return end
+
+    CAMI.RegisterPrivilege( {
+        Name = "GLide::RepairSWEP",
+        MinAccess = "admin"
+    } )
+end )
+
+local function IsAccess( ply, veh )
+    if game.SinglePlayer() then
+        return true
+    end
+
+    if not CAMI or not CAMI.PlayerHasAccess then
+        return ply:IsAdmin()
+    elseif CAMI.PlayerHasAccess( ply, "GLide::RepairSWEP" ) then
+        return true
+    end
+
+    local Access = hook.Run( "GLide_RepairSWEP::CanAccess", ply, veh )
+    if Access == nil then
+        return false
+    end
+
+    return false
+end
+
+function SWEP:Reload()
+    local user = self:GetOwner()
+    if not IsValid( user ) then return end
+
+    self:SetNextPrimaryFire( CurTime() + 0.1 )
+
+    if not SERVER then return end
+
+    local ent = self.repairTarget
+    if not ent then return end
+
+    if not IsAccess( user, ent ) then return end
+
+    local wasHealthIncreased, hasFinished = Glide.PartialRepair( ent, 99999, 1.0, user )
+
+    if wasHealthIncreased then
+        if user.ViewPunch then
+            user:ViewPunch( Angle( -5, 0, 0 ) )
+        end
+
+        self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
+        user:SetAnimation( PLAYER_ATTACK1 )
+
+        local trace = self.repairTrace
+
+        if trace then
+            local data = EffectData()
+            data:SetOrigin( trace.HitPos + trace.HitNormal * 5 )
+            data:SetNormal( trace.HitNormal )
+            data:SetScale( 1 )
+            data:SetMagnitude( 1 )
+            data:SetRadius( 10 )
+            util.Effect( "cball_bounce", data, false, true )
+        end
+    end
+
+    if hasFinished then
+        user:EmitSound( "buttons/lever6.wav", 75, math.random( 70, 80 ), 0.5 )
+    end
+end
+
 if not CLIENT then return end
 
 function SWEP:DrawHUD()
