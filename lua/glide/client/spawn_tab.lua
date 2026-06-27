49e554aec43816829b4e5e186d505f136f102100
StyledStrike <lameulucas@gmail.com>
2026-06-26 21:52:30 -0300

    Add search provider for Glide vehicles, supersedes #311

--- lua/glide/client/spawn_tab.lua
+++ lua/glide/client/spawn_tab.lua
@@ -59,3 +59,65 @@ hook.Add( "PopulateVehicles", "Glide.PopulateVehicles", function( panel, tree )
         Glide.Config:OpenFrame()
     end
 end )
+
+local IsTable = istable
+local IsString = isstring
+local Find = string.find
+
+local function DoesVehicleMatchSearch( data, className, searchString )
+    if not IsTable( data ) then
+        return false
+    end
+
+    local name = data.PrintName or data.Name
+
+    if IsString( name ) and Find( name:lower(), searchString, nil, true ) then
+        return true
+    end
+
+    if IsString( className ) and Find( className:lower(), searchString, nil, true ) then
+        return true
+    end
+
+    return false
+end
+
+search.AddProvider( function( searchString )
+    -- Get the search results limit.
+    -- As the convar itself describes it, this value is not a "hard limit" on the
+    -- total amount of results, instead it's used to limit different types of search results.
+    -- "Model amount limited to 1/2 of this value, entities are limited to 1/4".
+    -- We use the "entities" limit for Glide vehicles.
+    local maxSearchResults = GetConVar( "sbox_search_maxresults" ):GetInt() / 4
+
+    local results = {}
+    local count = 0
+
+    for className, data in pairs( list.Get( "GlideVehicles" ) ) do
+        if count > maxSearchResults then
+            break
+        end
+
+        if DoesVehicleMatchSearch( data, className, searchString ) then
+            local niceName = data.PrintName or data.Name or className
+
+            local contentIconData = {
+                nicename = niceName,
+                spawnname = className,
+                material = "entities/" .. className .. ".png",
+                admin = data.AdminOnly
+            }
+
+            count = count + 1
+            results[count] = {
+                text = className, -- Text to "Copy to clipboard"
+                icon = spawnmenu.CreateContentIcon( "entity", nil, contentIconData ),
+                words = { niceName, className }
+            }
+        end
+    end
+
+    table.SortByMember( results, "text", true )
+
+    return results
+end, "glide_vehicles_provider" )
