0fc17fd1d738c2d5f86b3be639e33406d6967a48
Vitroze <azouaouiaziz4@gmail.com>
2026-07-08 01:38:49 +0200

    Modified the Glide.SocketConnect function to align with the algorithm level. Removed the timer.

--- lua/glide/server/sockets.lua
+++ lua/glide/server/sockets.lua
@@ -7,41 +7,62 @@
 ]]
 
 --- Utility function to connect two vehicle sockets together.
-function Glide.SocketConnect( plug, receptacle, forceLimit )
-    local plugVeh = plug.vehicle
-    local receptacleVeh = receptacle.vehicle
+function Glide.SocketConnect( socketPlug, socketReceptacle, forceLimit )
+    local plugVeh = socketPlug:GetParent()
+    local receptacleVeh = socketReceptacle:GetParent()
 
     -- Make sure both vehicles are valid
     if not IsValid( plugVeh ) then return end
     if not IsValid( receptacleVeh ) then return end
 
     -- Remove existing plug constaint
-    if IsValid( plug.constraint ) then
-        plug.constraint:Remove()
+    if IsValid( socketPlug.constraint ) then
+        socketPlug.constraint:Remove()
     end
 
     -- Remove existing receptacle constaint
-    if IsValid( receptacle.constraint ) then
-        receptacle.constraint:Remove()
+    if IsValid( socketReceptacle.constraint ) then
+        socketReceptacle.constraint:Remove()
     end
 
     -- Try to create a ballsocket constaint
-    local constr = constraint.Ballsocket( plugVeh, receptacleVeh, 0, 0, receptacle.offset, forceLimit, 0, 0 )
+    local constr = constraint.Ballsocket( plugVeh, receptacleVeh, 0, 0, socketReceptacle.vecOffset, forceLimit, 0, 0 )
     if not IsValid( constr ) then return end
 
     constr.DoNotDuplicate = true
     constr.DisableDuplicator = true
 
+    constr:CallOnRemove( "Glide.SocketDisconnect", function()
+        if IsValid( socketReceptacle ) then
+            socketReceptacle.constraint = nil
+        end
+
+        if IsValid( plugVeh ) then
+            plugVeh:UpdateSocketCount()
+            plugVeh:OnSocketDisconnect( socketPlug )
+        end
+
+        if IsValid( socketPlug ) then
+            socketPlug.constraint = nil
+            socketPlug.nextAttemptTime = CurTime() + 3
+        end
+
+        if IsValid( receptacleVeh ) then
+            receptacleVeh:UpdateSocketCount()
+            receptacleVeh:OnSocketDisconnect( socketReceptacle )
+        end
+    end )
+
     -- Store constraint on both sockets
-    plug.constraint = constr
-    receptacle.constraint = constr
+    socketPlug.constraint = constr
+    socketReceptacle.constraint = constr
 
     -- Call events on both vehicles
     plugVeh:UpdateSocketCount()
     receptacleVeh:UpdateSocketCount()
 
-    plugVeh:OnSocketConnect( plug, receptacleVeh )
-    receptacleVeh:OnSocketConnect( receptacle, plugVeh )
+    plugVeh:OnSocketConnect( socketPlug, receptacleVeh )
+    receptacleVeh:OnSocketConnect( socketReceptacle, plugVeh )
 end
 
 local vehiclesWithSockets = Glide.vehiclesWithSockets or {}
@@ -73,77 +94,3 @@ do
         return closestSocket, closestDist
     end
 end
-
-local IsValid = IsValid
-local Remove = table.remove
-
-local FindClosestSocket = Glide.FindClosestSocket
-local GetDevMode = Glide.GetDevMode
-
-timer.Create( "Glide.UpdateSockets", 0.1, 0, function()
-    local vehicleCount = #vehiclesWithSockets
-    if vehicleCount == 0 then return end
-
-    local receptacles, plugs = {}, {}
-    local rCount, pCount = 0, 0
-    local vehicle
-
-    for i = vehicleCount, 1, -1 do
-        vehicle = vehiclesWithSockets[i]
-
-        if IsValid( vehicle ) then
-            for socketIndex, socket in ipairs( vehicle.Sockets ) do
-
-                -- Update the socket's current world position
-                socket.pos = vehicle:LocalToWorld( socket.offset )
-
-                -- We need to know which vehicle owns this socket on the loop below
-                socket.index = socketIndex
-                socket.vehicle = vehicle
-
-                -- Separate receptacle/plug sockets from this vehicle
-                if socket.isReceptacle == true then
-                    rCount = rCount + 1
-                    receptacles[rCount] = socket
-                else
-                    pCount = pCount + 1
-                    plugs[pCount] = socket
-                end
-
-            end
-        else
-            -- Remove invalid vehicles
-            Remove( vehiclesWithSockets, i )
-        end
-    end
-
-    -- For each plug...
-    for _, plug in ipairs( plugs ) do
-        vehicle = plug.vehicle
-
-        -- If this plug is not connected yet...
-        if not IsValid( plug.constraint ) then
-            -- Find the closest receptacle to this plug
-            local receptacle = FindClosestSocket( plug.pos, 80, plug.id, receptacles )
-
-            if receptacle then
-                -- Let the vehicle with this plug try to connect
-                -- with the nearby receptacle.
-                plug.attemptReceptacle = receptacle
-            end
-        end
-    end
-
-    -- Draw debug overlays, if `developer` cvar is active
-    if not GetDevMode() then return end
-
-    for _, v in ipairs( receptacles ) do
-        debugoverlay.Cross( v.pos, 8, 0.1, Color( 255, 145, 0 ), true )
-        debugoverlay.Text( v.pos, v.id .. " | isReceptacle: true", 0.1, false )
-    end
-
-    for _, v in ipairs( plugs ) do
-        debugoverlay.Cross( v.pos, 8, 0.1, Color( 255, 145, 0 ), true )
-        debugoverlay.Text( v.pos, v.id .. " | isReceptacle: false", 0.1, false )
-    end
-end )
