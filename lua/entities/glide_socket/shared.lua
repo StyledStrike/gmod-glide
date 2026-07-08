9d214a0069ae280fc8bd48162278ad7bf39d6e31
Vitroze <azouaouiaziz4@gmail.com>
2026-07-08 19:49:46 +0200

    Removal of debugging tools

--- lua/entities/glide_socket/shared.lua
+++ lua/entities/glide_socket/shared.lua
@@ -10,8 +10,3 @@ ENT.AdminOnly = true
 ENT.PhysgunDisabled = true
 ENT.DoNotDuplicate = true
 ENT.DisableDuplicator = true
-
--- TODO: Remove the radius NetworkVar and use the radius property instead
-function ENT:SetupDataTables()
-    self:NetworkVar( "Float", 0, "RadiusDev" )
-end
